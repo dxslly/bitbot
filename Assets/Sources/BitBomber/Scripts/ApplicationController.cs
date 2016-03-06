c0394159678fab3cf50f5b7647088e9af6f5f624
David Samuelson <me@dsamuelson.com>
2016-03-06 05:08:18 -0500

    ADDED: Improved error checking for ai scripts.

--- Assets/Sources/BitBomber/Scripts/ApplicationController.cs
+++ Assets/Sources/BitBomber/Scripts/ApplicationController.cs
@@ -3,10 +3,10 @@ using BitBots.BitBomber.Features.Tiles;
 using BitBots.BitBomber.Features.View;
 using BitBots.BitBomber.Features.GameTick;
 using BitBots.BitBomber.Features.PlayerAI;
+using BitBots.BitBomber.Features.Player;
 using BitBots.BitBomber.Features.Movement;
 using Entitas;
 using Entitas.Unity.VisualDebugging;
-using Jint;
 using UnityEngine;
 
 namespace BitBots.BitBomber
@@ -32,40 +32,8 @@ namespace BitBots.BitBomber
         
         private void CreateAI(Pool pool)
         {
-            // Load AI From
-            TextAsset blueTextAsset = Resources.Load<TextAsset>("BitBomber/PlayerAIs/SimpleAI");
-            if (null == blueTextAsset)
-            {
-                Debug.LogWarning("Unable to load AI Script");
-                return;
-            }
-            
-            JintEngine blueEngine = new JintEngine();
-            blueEngine.Run(blueTextAsset.text);
-            
-            // Blue Player
-            pool.CreateEntity()
-                .AddTilePosition(1, 1)
-                .AddPlayer("Blue Player")
-                .AddPlayerAI(blueEngine)
-                .AddPrefab(Res.bluePlayer);
-                
-            TextAsset redTextAsset = Resources.Load<TextAsset>("BitBomber/PlayerAIs/RandomAI");
-            if (null == redTextAsset)
-            {
-                Debug.LogWarning("Unable to load AI Script");
-                return;
-            }
-                
-            JintEngine redEngine = new JintEngine();
-            redEngine.Run(redTextAsset.text);
-            
-            // Red Player
-            pool.CreateEntity()
-                .AddTilePosition(8, 8)
-                .AddPlayer("Red Player")
-                .AddPlayerAI(redEngine)
-                .AddPrefab(Res.redPlayer);
+            pool.CreateAIPlayer(1, 1, PlayerColor.Red, "BitBomber/PlayerAIs/RandomAI");
+            pool.CreateAIPlayer(8, 8, PlayerColor.Blue, "BitBomber/PlayerAIs/RandomAI");
         }
         
         private Systems CreateSystems(Pool pool)
