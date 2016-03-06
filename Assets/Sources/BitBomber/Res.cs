f2cb6d03bc09a52bb97d898923425dd4f8dd5c5a
David Samuelson <me@dsamuelson.com>
2016-03-05 20:35:03 -0500

    ADDED: New tilemap art

--- Assets/Sources/BitBomber/Res.cs
+++ Assets/Sources/BitBomber/Res.cs
@@ -5,9 +5,9 @@ namespace BitBots.BitBomber
 {
     public static class Res
     {
-        public const string walkableTile01 = "BitBomber/Prefabs/Tiles/DebugTile";
-        public const string walkableTile02 = "BitBomber/Prefabs/Tiles/DebugTile";
-        public const string walkableTile03 = "BitBomber/Prefabs/Tiles/DebugTile";
-        public const string walkableTile04 = "BitBomber/Prefabs/Tiles/DebugTile";
+        public const string walkableTile01 = "BitBomber/Prefabs/Tiles/WalkableTile01";
+        public const string walkableTile02 = "BitBomber/Prefabs/Tiles/WalkableTile01";
+        public const string walkableTile03 = "BitBomber/Prefabs/Tiles/WalkableTile01";
+        public const string walkableTile04 = "BitBomber/Prefabs/Tiles/WalkableTile01";
     }
 }
